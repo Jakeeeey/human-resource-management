e1feda7e054877751730404b76ab32f6ee73f412
Bautista <168814963+Nazeeee0214@users.noreply.github.com>
2026-04-22 16:47:05 +0800

    Update ThemeProvider.tsx

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -9,17 +9,6 @@ export default function ThemeProvider({
 }: {
     children: React.ReactNode;
 }) {
-    const [mounted, setMounted] = React.useState(false);
-
-    React.useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    // Prevent hydration error/mismatch in React 19
-    if (!mounted) {
-        return <>{children}</>;
-    }
-
     return (
         <NextThemesProvider
             attribute="class"
