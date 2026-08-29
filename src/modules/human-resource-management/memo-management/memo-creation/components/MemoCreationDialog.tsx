fbaf69f2b0b41858f49f4f24c05a83df128fe67b
Erman <66209038+Xerman09@users.noreply.github.com>
2026-08-29 16:51:20 +0800

    fix ci issue

--- src/modules/human-resource-management/memo-management/memo-creation/components/MemoCreationDialog.tsx
+++ src/modules/human-resource-management/memo-management/memo-creation/components/MemoCreationDialog.tsx
@@ -7,7 +7,8 @@ import { MemoCreationService } from "../services/MemoCreationService";
 import { memoFormSchema, MemoFormValues } from "../types/memo-creation.schema";
 import { toast } from "sonner";
 import dynamic from "next/dynamic";
-const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+// @ts-ignore - react-quill-new missing types in CI
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false }) as any;
 import "react-quill-new/dist/quill.snow.css";
 
 import {
