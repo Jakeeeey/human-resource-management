3371c670384acea70f64eb34173c2848d32eed1f
Erman <66209038+Xerman09@users.noreply.github.com>
2026-09-05 13:47:21 +0800

    fix error

--- src/modules/human-resource-management/memo-management/memo-creation/components/MemoCreationDialog.tsx
+++ src/modules/human-resource-management/memo-management/memo-creation/components/MemoCreationDialog.tsx
@@ -7,7 +7,6 @@ import { MemoCreationService } from "../services/MemoCreationService";
 import { memoFormSchema, MemoFormValues } from "../types/memo-creation.schema";
 import { toast } from "sonner";
 import dynamic from "next/dynamic";
-// @ts-expect-error - react-quill-new missing types in CI
 const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false }) as React.ElementType;
 import "react-quill-new/dist/quill.snow.css";
 
